1746bdd32ab3f90189d734ef9ac0322234266b52
Adrian Wan <awan1@cs.swarthmore.edu>
2014-12-02 20:26:16 -0500

    Adding files for a formatted version of the page

--- project/webpage/static/hbase-link-test.py
+++ project/webpage/static/hbase-link-test.py
@@ -35,15 +35,16 @@ def main():
     text = tweet_dict['text']
     in_reply_to_status_id = tweet_dict['in_reply_to_status_id']
     in_reply_to_user_id = tweet_dict['in_reply_to_user_id']
-    retweet_count = tweet_dict['retweet_count']
+    retweet_count = tweet_dict['retweet_count'] + 1
 
     url = "http://www.twitter.com/{}/status/{}".format(username, tweet_id)
 
     # Add it to graph as a node
     G.add_node(node_name, retweet_count=retweet_count, url=url, text=text)
 
-    # Add its edges
-    G.add_edge(tweet_id, in_reply_to_status_id)
+    # If it's in reply to something in the graph, add the edge.
+    if in_reply_to_status_id in G:
+      G.add_edge(tweet_id, in_reply_to_status_id)
 
   # Dump to JSON
   data = json_graph.node_link_data(G)
